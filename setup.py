3a113cd01cf244efb8b8daf0202e418be8ba987b
FriedRice <n.mclaughlin22@yahoo.com>
2013-06-04 10:30:55 -0400

    Renamed directory and updated setup files.

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django-wymeditor',
-    version='0.0.1',
-    author=u'',
+    name='djangocms_wymeditor_plugin',
+    version='0.1.0',
+    author='',
     author_email='',
-    packages=['django_wymeditor',],
-    url='',
-    license='BSD licence, see LICENCE.txt',
-    description='',
+    packages=['djangocms_wymeditor_plugin',],
+    url='https://github.com/FriedRice/djangocms_wymeditor_plugin.git',
+    license='',
+    description='A plugin for using WYMeditor in Django CMS',
     include_package_data=True,
     zip_safe=False,
 )
